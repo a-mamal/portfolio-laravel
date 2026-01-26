29fa8a6620d276a4bc632f1116307929ecf8171f
a-mamal <mamalikidoua@gmail.com>
2026-01-26 18:43:29 +0200

    feat(layout): create Layout component and add custom layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ $title ?? config('app.name', 'Mamalikidou Anastasia') }}</title>
+        <meta name="description" content="Portfolio">
+        <meta name="theme-color" content="#555">
+        @vite('resources/css/app.css')
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+        @vite('resources/js/app.js')
+        <script src="{{ asset('js/main.js') }}" defer></script>
+    </head>
+    <body>
+        {{-- This navigation include is commented out for now --}}
+        {{-- @include('layouts.navigation') --}}
+
+        @include('partials.header')
+
+        <!-- Page Heading -->
+        @isset($header)
+            <header>
+                {{ $header }}
+            </header>
+        @endisset
+
+        <div class="right-wrapper">
+            <main>
+                {{ $slot }}
+            </main>
+
+            <footer>
+                @include('partials.footer')
+            </footer>
+        </div>
+    </body>
+</html>
