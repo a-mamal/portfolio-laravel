f584e3239ffdc00ab335931b364abf15d497efc2
a-mamal <mamalikidoua@gmail.com>
2026-01-28 14:50:05 +0200

    feat(db): add user_id (FK), display_name, bio, image columns to profiles table migration

--- database/migrations/2026_01_28_123842_create_profiles_table.php
+++ database/migrations/2026_01_28_123842_create_profiles_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')           
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('display_name');        // Public name
+            $table->text('bio')->nullable();   
+            $table->string('image')->nullable(); 
             $table->timestamps();
         });
     }
